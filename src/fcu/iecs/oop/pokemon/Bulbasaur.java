ee5bada4fae9f37bbd03688f3ce6de0a2ac35b62
Z <Z@192.168.0.100>
2016-12-28 05:48:54 +0800

    8

---
+++ src/fcu/iecs/oop/pokemon/Bulbasaur.java
@@ -0,0 +1,22 @@
+package fcu.iecs.oop.pokemon;
+
+public class Bulbasaur extends Pokemon {
+
+public Bulbasaur(String nickName, PokemonType type, int cp){
+		
+		super(nickName, type, cp);
+	}
+	
+	
+	
+	
+	public void attack() {
+		
+		
+		
+		
+		System.out.println("Tackle..");
+
+	}
+
+}
